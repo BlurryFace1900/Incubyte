fda4c7727fbc630a1f057b637f00901a13c8be43
BlurryFace1900 <debojitchowdhury98@gmail.com>
2021-04-25 15:08:53 +0000

    two numbers string delimited by new line test pass

--- main/StringCalculator.java
+++ main/StringCalculator.java
@@ -5,7 +5,7 @@ public class StringCalculator {
 	public int add(String number){
         if(number.length() == 1) return Integer.parseInt(number);
         if(number.length() > 1){
-			String a[] = number.split(",");
+			String a[] = number.split(",|\n");
 			return Integer.parseInt(a[0]) + Integer.parseInt(a[1]);
 		}
 		return 0;
