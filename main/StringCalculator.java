8b494b3ddf8037260c8e7278d067cb87856c9316
BlurryFace1900 <debojitchowdhury98@gmail.com>
2021-04-25 14:40:15 +0000

    two numbers string test pass

--- main/StringCalculator.java
+++ main/StringCalculator.java
@@ -4,6 +4,10 @@ public class StringCalculator {
 	
 	public int add(String number){
         if(number.length() == 1) return Integer.parseInt(number);
+        if(number.length() > 1){
+			String a[] = number.split(",");
+			return Integer.parseInt(a[0]) + Integer.parseInt(a[1]);
+		}
 		return 0;
 	}
 
